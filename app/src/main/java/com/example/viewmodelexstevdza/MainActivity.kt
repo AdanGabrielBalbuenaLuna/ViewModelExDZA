6e28d2f2a3a133b7b377d6795b491dce9e3a9ebd
GabrielWindows <agbalbuenaluna@gmail.com>
2025-09-02 14:08:37 -0600

    [VIEWMODEL-EXAMPLE] This code has an example how to implement a basic ViewModel, check notebook notes for more info

--- app/src/main/java/com/example/viewmodelexstevdza/MainActivity.kt
+++ app/src/main/java/com/example/viewmodelexstevdza/MainActivity.kt
@@ -1,21 +1,28 @@
-
 package com.example.viewmodelexstevdza
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        var button = findViewById<Button>(R.id.button)
+        var textView: TextView = findViewById(R.id.counter)
+
+        var viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        //textView.text = viewModel.number.toString()
+
+        button.setOnClickListener {
+            viewModel.addNumber()
+            textView.text = viewModel.number.toString()
         }
     }
 }
